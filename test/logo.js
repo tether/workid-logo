66d5cee28b282b6d6c059c6e1b85a86bf33b1892
bredele <olivier.wietrich@gmail.com>
2017-05-28 20:52:33 -0600

    Add first draft with garbage styles

---
+++ test/logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Logo from '..'
+
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+ReactDOM.render(
+  <Logo/>,
+  root
+)
